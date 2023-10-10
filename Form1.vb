311fa8803e50817789297c3cd9636dc4b2554606
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 09:32:19 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = Int(VBMath.Rnd() * 6) + 1
+    End Sub
+End Class
